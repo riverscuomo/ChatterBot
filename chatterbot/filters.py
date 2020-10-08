2daf5722ce6d90dd4ac10dceeaefc39ce764184c
Rivers Cuomo <riverscuomo@users.noreply.github.com>
2020-10-07 17:01:59 -0700

    Update filters.py

--- chatterbot/filters.py
+++ chatterbot/filters.py
@@ -1,7 +1,13 @@
-def get_recent_repeated_responses(chatbot, conversation, sample=10, threshold=3, quantity=3):
+def get_recent_repeated_responses(chatbot, conversation, sample=500, threshold=1, quantity=500):
+# def get_recent_repeated_responses(chatbot, conversation, sample=10, threshold=3, quantity=3):
     """
     A filter that eliminates possibly repetitive responses to prevent
     a chat bot from repeating statements that it has recently said.
+
+    The score in the tuple is how many times it's been said in the sample group
+    you're looking at
+
+    I want to look through MANY lines of conversation to eliminate lines that are identical.
     """
     from collections import Counter
 
@@ -19,6 +25,8 @@ def get_recent_repeated_responses(chatbot, conversation, sample=10, threshold=3,
 
     # Find the n most common responses from the conversation
     most_common = counter.most_common(quantity)
+    print("10 most_common:")
+    print(most_common[:10])
 
     return [
         counted[0] for counted in most_common
